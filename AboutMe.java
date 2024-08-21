3ad69e2570a43b557c435234ec60741fa4c0f508
TannerCantwell <tanner.cantwell@mcusd709.org>
2024-08-21 09:17:54 -0500

    created about me

---
+++ AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+    public static void main(String[] args) {
+      System.out.println("My name is Tanner Cantwell."); 
+      System.out.println("I really like banana bread."); 
+      System.out.println("I am 6'2."); 
+    }
+}
